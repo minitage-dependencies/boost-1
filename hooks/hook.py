44ed389ac04d240bad11473c75948166590b6812
kiorky <kiorky@cryptelium.net>
2011-03-31 22:51:26 +0200

    init

--- hooks/hook.py
+++ hooks/hook.py
@@ -15,7 +15,49 @@
 __docformat__ = 'restructuredtext en'
 
 import re
+import platform
 import os
+import sys
+
+
+from  subprocess import Popen, PIPE
+from minitage.core.common import which
+
+
+
+def get_install_name_tool():
+    if sys.platform.lower() == 'darwin':
+        return which('install_name_tool')
+
+def get_otool():
+    if sys.platform.lower() == 'darwin':
+        return which('otool')
+
+
+def get_rpath(lib):
+    intool = get_otool()
+    output = Popen([intool, '-L', lib], stdout=PIPE, stderr=PIPE)
+    output.wait()
+    content = output.stdout.read()
+    rlibs = []
+    if len(content.splitlines())>2:
+        rlibs = [b[0] for b in [a.split() for a in content.splitlines()[1:]]]
+    return rlibs
+
+def relink(options, buildout):
+    libdir = os.path.join(
+        buildout['part']['location'], 'lib'
+    )
+    if sys.platform.lower() == 'darwin':
+        libs = [a for a in os.listdir(libdir) if 'dylib' in a]
+        for i in libs:
+            ip = os.path.join(libdir, i)
+            os.system('install_name_tool -id %s %s' % (ip, ip))
+            rlibs = get_rpath(ip)
+            for lib in rlibs:
+                jp = os.path.join(libdir, lib)
+                if lib in libs:
+                    os.system('install_name_tool -change %s %s %s' % (lib, jp, ip))
 
 def hook(options=None,buildout=None):
     if not options:
@@ -38,6 +80,7 @@ def hook(options=None,buildout=None):
     fp = os.path.join(buildout['buildout']['directory'], 'user-config.jam')
     if os.path.exists(fp):
         os.remove(fp)
+    relink(options, buildout)
 
 if __name__ == '__main__':
     hook()
